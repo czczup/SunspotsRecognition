0a0c77ef71573e068f95585452fbcff83b8e3bb3
czczup <wztxy89@163.com>
2020-06-16 23:54:46 +0800

    改回了tf1.9

---
+++ model.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+from functools import reduce
+from operator import mul
+
+
+class Model(object):
+    def __init__(self):
+        self.image = tf.placeholder(tf.float32, [None, None, None, 3], name='image')
+
+        with tf.name_scope("label"):
+            self.label = tf.placeholder(tf.int32, [None], name='label')
+            self.one_hot = tf.one_hot(indices=self.label, depth=3, name='one_hot')
+
+        self.global_step = tf.Variable(0, name="global_step", trainable=False)
+        self.training = tf.placeholder(tf.bool)
+
+        self.output = self.network(self.image)
+
+        self.prediction = tf.layers.dense(self.output, units=3)
+
+        self.loss = self.get_loss(self.prediction, self.one_hot)
+
+        self.batch_size = 32
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.argmax(self.prediction, 1), tf.argmax(self.one_hot, 1))
+        with tf.name_scope('accuracy'):
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            tf.summary.scalar('train_accuracy', self.accuracy)
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            self.optimizer = tf.train.AdamOptimizer(1e-3).minimize(self.loss, global_step=self.global_step)
+
+        self.merged = tf.summary.merge_all()
+        print("网络初始化成功")
+
+    def conv2d(self, x, input_filters, output_filters, kernel, strides=1, padding="SAME"):
+        with tf.name_scope('conv'):
+            shape = [kernel, kernel, input_filters, output_filters]
+            weight = tf.Variable(tf.truncated_normal(shape, stddev=0.1), name='weight')
+            return tf.nn.conv2d(x, weight, strides=[1, strides, strides, 1], padding=padding, name='conv')
+
+    def residual(self, x, num_filters, strides, with_shortcut=False):
+        with tf.name_scope('residual'):
+            conv1 = self.conv2d(x, num_filters[0], num_filters[1], kernel=1, strides=strides)
+            bn1 = tf.layers.batch_normalization(conv1, axis=3, training=self.training)
+            relu1 = tf.nn.relu(bn1)
+            conv2 = self.conv2d(relu1, num_filters[1], num_filters[2], kernel=3)
+            bn2 = tf.layers.batch_normalization(conv2, axis=3, training=self.training)
+            relu2 = tf.nn.relu(bn2)
+            conv3 = self.conv2d(relu2, num_filters[2], num_filters[3], kernel=1)
+            bn3 = tf.layers.batch_normalization(conv3, axis=3, training=self.training)
+            if with_shortcut:
+                shortcut = self.conv2d(x, num_filters[0], num_filters[3], kernel=1, strides=strides)
+                bn_shortcut = tf.layers.batch_normalization(shortcut, axis=3, training=self.training)
+                residual = tf.nn.relu(bn_shortcut+bn3)
+            else:
+                residual = tf.nn.relu(x+bn3)
+        return residual
+
+    def network(self, image):
+        with tf.name_scope("ResNet"):
+            with tf.name_scope('stage1'):
+                conv = self.conv2d(image, 3, 32, 7, 1)
+                bn = tf.layers.batch_normalization(conv, axis=3, training=self.training)
+                relu = tf.nn.relu(bn)
+                print(relu)
+            with tf.name_scope('stage2'):
+                pool = tf.nn.max_pool(relu, [1, 3, 3, 1], [1, 2, 2, 1], padding="SAME")
+                res = self.residual(pool, [32, 16, 16, 64], 1, with_shortcut=True)
+                print(res)
+            with tf.name_scope('stage3'):
+                res = self.residual(res, [64, 32, 32, 128], 2, with_shortcut=True)
+                print(res)
+            with tf.name_scope('stage4'):
+                res = self.residual(res, [128, 64, 64, 256], 2, with_shortcut=True)
+                print(res)
+            with tf.name_scope('stage5'):
+                res = self.residual(res, [256, 128, 128, 512], 2, with_shortcut=True)
+            with tf.name_scope('stage6'):
+                res = self.residual(res, [512, 256, 256, 1024], 2, with_shortcut=True)
+                print(res)
+                pool = self.global_max_pooling(res)
+                print(pool)
+        return pool
+
+    def global_max_pooling(self, x):
+        return tf.reduce_max(x, [1, 2])
+
+    def get_loss(self, output_concat, onehot):
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=output_concat, labels=onehot)
+            loss = tf.reduce_mean(losses)
+            tf.summary.scalar('loss', loss)
+        return loss
+
+    def get_num_params(self):
+        num_params = 0
+        for variable in tf.trainable_variables():
+            shape = variable.get_shape()
+            num_params += reduce(mul, [dim.value for dim in shape], 1)
+        return num_params
+
+
+if __name__ == '__main__':
+    model = Model()
+    print(model.get_num_params())
--- train.py
+++ train.py
@@ -1,78 +1,116 @@
+from model import Model
 import tensorflow as tf
+import time
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '4'
-
-train_reader = tf.data.TFRecordDataset('dataset/tfrecord/train_avgpool.tfrecord')
-valid_reader = tf.data.TFRecordDataset('dataset/tfrecord/valid_avgpool.tfrecord')
-
-feature_description = {
-    'image': tf.io.FixedLenFeature([], tf.string),
-    'label': tf.io.FixedLenFeature([], tf.int64),
-    'width': tf.io.FixedLenFeature([], tf.int64),
-    'height': tf.io.FixedLenFeature([], tf.int64)
-}
 
 
-def _parse_function(exam_proto):
-    temp = tf.io.parse_single_example(exam_proto, feature_description)
-    image = tf.io.decode_raw(temp['image'], tf.float32)
-    label = temp['label']
-    width, height = temp['width'], temp['height']
-    image = tf.reshape(image, [width, height, 3])
+def read_and_decode(filename):
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    feature_description = {
+        'image': tf.FixedLenFeature([], tf.string),
+        'label': tf.FixedLenFeature([], tf.int64),
+        'width': tf.FixedLenFeature([], tf.int64),
+        'height': tf.FixedLenFeature([], tf.int64)
+    }
+    temp  = tf.parse_single_example(serialized_example, feature_description)
+    image = tf.decode_raw(temp['image'], tf.float32)
+    width = tf.cast(temp['width'], tf.int64)
+    height = tf.cast(temp['height'], tf.int64)
+
+    image = tf.reshape(image, tf.convert_to_tensor([width, height, 3]))
+    # image = tf.image.random_flip_left_right(image) # 随机左右翻转
+    # image = tf.image.random_flip_up_down(image) # 随机上下翻转
+    label = tf.cast(temp['label'], tf.int64)
     return image, label
 
 
-BATCH_SIZE = 32
-DATASET_SIZE = 19689
-EPOCHS = 200
-train_dataset = train_reader.repeat(EPOCHS).shuffle(2560, reshuffle_each_iteration=True).\
-    map(_parse_function).padded_batch(BATCH_SIZE, padded_shapes=([None, None, 3], []))
-valid_dataset = valid_reader.shuffle(2560, reshuffle_each_iteration=True).\
-    map(_parse_function).padded_batch(BATCH_SIZE, padded_shapes=([None, None, 3], []))
+def load_train_set():
+    with tf.name_scope('input_train'):
+        image_train, label_train = read_and_decode("dataset/tfrecord/train_avgpool.tfrecord")
+        image_batch_train, label_batch_train = tf.train.batch(
+            [image_train, label_train], batch_size=batch_size, capacity=2048, num_threads=4, dynamic_pad=True
+        )
+    return image_batch_train, label_batch_train
 
 
-base_model = tf.keras.applications.ResNet50(weights="imagenet", include_top=False)
+def load_valid_set():
+    with tf.name_scope('input_valid'):
+        image_valid, label_valid = read_and_decode("dataset/tfrecord/valid_avgpool.tfrecord")
+        image_batch_valid, label_batch_valid = tf.train.batch(
+            [image_valid, label_valid], batch_size=batch_size, capacity=2048, num_threads=4, dynamic_pad=True
+        )
+    return image_batch_valid, label_batch_valid
 
-base_model.trainable = False
-global_maxpool_layer = tf.keras.layers.GlobalMaxPooling2D()
-prediction_layer = tf.keras.layers.Dense(3)
-# model = tf.keras.Sequential([
-#     base_model,
-#     global_maxpool_layer,
-#     prediction_layer
-# ])
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), padding='same'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.MaxPool2D((2, 2)),
-    tf.keras.layers.Conv2D(64, (3, 3), padding='same'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.MaxPool2D((2, 2)),
-    tf.keras.layers.Conv2D(128, (3, 3), padding='same'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.MaxPool2D((2, 2)),
-    tf.keras.layers.Conv2D(256, (3, 3), padding='same'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.MaxPool2D((2, 2)),
-    tf.keras.layers.Conv2D(512, (3, 3), padding='same'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.GlobalMaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(3)
-])
-model.build(input_shape=(None, None, None, 3))
-model.summary()
-model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-            optimizer = tf.keras.optimizers.Adam(lr=1E-3),
-            metrics=['accuracy'])
+def train(model):
+    # network
+    amount = 19689
+    image_batch_train, label_batch_train = load_train_set()
+    image_batch_valid, label_batch_valid = load_valid_set()
 
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="logs/mymodel-gmp-lr0.001/", update_freq=1)
+    # Adaptive use of GPU memory.
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
 
-history = model.fit(train_dataset, epochs=EPOCHS, steps_per_epoch=DATASET_SIZE//BATCH_SIZE,
-                    validation_data=valid_dataset, callbacks=[tensorboard_callback])
+    with tf.Session(config=tf_config) as sess:
+        # general setting
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.global_variables_initializer())
+        # Recording training process.
+        writer_train = tf.summary.FileWriter("models/"+dirId+"/logs/train", sess.graph)
+        writer_valid = tf.summary.FileWriter("models/"+dirId+"/logs/valid", sess.graph)
+
+        last_file = tf.train.latest_checkpoint("models/"+dirId)
+        var_list = [var for var in tf.global_variables() if "moving" in var.name]
+        var_list += [var for var in tf.global_variables() if "global_step" in var.name]
+        var_list += tf.trainable_variables()
+        saver = tf.train.Saver(var_list=var_list, max_to_keep=5)
+        if last_file:
+            tf.logging.info('Restoring model from {}'.format(last_file))
+            saver.restore(sess, last_file)
+        # train
+        while True:
+            time1 = time.time()
+            image_train, label_train, step = sess.run(
+                [image_batch_train, label_batch_train, model.global_step])
+            _, loss_ = sess.run([model.optimizer, model.loss], feed_dict={model.image: image_train,
+                                                                          model.label: label_train,
+                                                                          model.training: True})
+            print('[epoch %d, step %d/%d]: loss %.6f' % (
+            step // (amount // batch_size), step % (amount // batch_size), amount // batch_size, loss_),
+                  'time %.3fs' % (time.time() - time1))
+            if step % 10 == 0:
+                image_train, label_train = sess.run([image_batch_train, label_batch_train])
+                acc_train, summary = sess.run([model.accuracy, model.merged], feed_dict={model.image: image_train,
+                                                                                         model.label: label_train,
+                                                                                         model.training: True})
+                writer_train.add_summary(summary, step)
+                image_valid, label_valid = sess.run([image_batch_valid, label_batch_valid])
+                acc_valid, summary = sess.run([model.accuracy, model.merged], feed_dict={model.image: image_valid,
+                                                                                         model.label: label_valid,
+                                                                                         model.training: True})
+                writer_valid.add_summary(summary, step)
+                print('[epoch %d, step %d/%d]: train acc %.3f, valid acc %.3f' % (step // (amount // batch_size),
+                                                                                  step % (amount // batch_size),
+                                                                                  amount // batch_size, acc_train,
+                                                                                  acc_valid),
+                      'time %.3fs' % (time.time() - time1))
+            if step % 100 == 0:
+                print("Save the model Successfully")
+                saver.save(sess, "models/"+dirId+"/model.ckpt", global_step=step)
+
+    coord.request_stop()
+    coord.join(threads)
+
+
+if __name__ == '__main__':
+    deviceId = input("device id: ")
+    os.environ["CUDA_VISIBLE_DEVICES"] = deviceId
+    dirId = input("dir id: ")
+    model = Model()
+    batch_size = model.batch_size
+    train(model)
